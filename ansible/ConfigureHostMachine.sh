edbccdeb1a53e8642b2c4521f6166790005635c8
David Wallis <david@wallis2000.co.uk>
2018-03-08 16:51:23 +0000

    rsyslog

--- ansible/ConfigureHostMachine.sh
+++ ansible/ConfigureHostMachine.sh
@@ -4,12 +4,17 @@ sudo apt-get update
 sudo apt-get install python-pip git python-dev sshpass -y
 sudo pip install ansible
 
+# Add modules
+# ansible-galaxy install sansible.rsyslog
+
 # Clone  repo:
 git clone https://github.com/davidwallis3101/HGConfigure.git
 cd HGConfigure/ansible/
 
 # Execute playbook
 # ./playbook.yml
+
+ansible-galaxy install -p ./roles -r roles.yml
 
 ansible-playbook playbook.yml -i 192.168.0.81, --ask-pass --become -c paramiko 
 
