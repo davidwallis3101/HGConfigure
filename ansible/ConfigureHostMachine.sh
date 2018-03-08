298519e0bb4141bfe6248d2501fdf2f51bb7b342
David Wallis <david@wallis2000.co.uk>
2018-03-08 13:13:08 +0000

    refactor

--- ansible/ConfigureHostMachine.sh
+++ ansible/ConfigureHostMachine.sh
@@ -3,3 +3,15 @@
 sudo apt-get update
 sudo apt-get install python-pip git python-dev sshpass -y
 sudo pip install ansible
+
+# Clone  repo:
+git clone https://github.com/davidwallis3101/HGConfigure.git
+cd HGConfigure/ansible/
+
+# Execute playbook
+# ./playbook.yml
+
+ansible-playbook playbook.yml -i 192.168.0.81, --ask-pass --become -c paramiko 
+
+## Diferent sudo password
+ansible-playbook playbook.yml -i 192.168.0.81, --ask-pass --become -c paramiko --ask-become-pass
